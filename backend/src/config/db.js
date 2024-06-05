71f6a4075350b76fbbb97551c226ad1517b1c8e4
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-06-04 23:50:24 -0700

    seting up backend

---
+++ backend/src/config/db.js
@@ -0,0 +1,17 @@
+//db.js is typically used to handle the configuration and connection to a database
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
