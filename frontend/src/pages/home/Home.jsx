348ec69052a502eeb5bd31d3b63dfff4ca831fd8
Jessie Le <phuong2508truc@gmail.com>
2024-07-06 16:17:16 -0700

    adding design for time and guest boxes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,12 +2,25 @@
 import React from "react";
 import { useState } from "react";
 
-  function Home() {
-  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleDropdown = (e) => {
+function Home() {
+  const [isRestaurantOpen, setIsRestaurantOpen] = useState(false);
+  const [isTimeOpen, setIsTimeOpen] = useState(false);
+  const [isGuestOpen, setIsGuestOpen] = useState(false);
+
+  const toggleRestaurantDropdown = (e) => {
     e.preventDefault();
-    setIsOpen(!isOpen);
+    setIsRestaurantOpen(!isRestaurantOpen);
+  };
+
+  const toggleTimeDropdown = (e) => {
+    e.preventDefault();
+    setIsTimeOpen(!isTimeOpen);
+  };
+
+  const toggleGuestDropdown = (e) => {
+    e.preventDefault();
+    setIsGuestOpen(!isGuestOpen);
   };
   return (
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -34,28 +47,57 @@ import { useState } from "react";
             </h3>
 
             <form className="RestaurantForm">
-              <div className="relative flex flex-col items-center border-solid border-[3px] border-orange-600 cursor-pointer font-bold">
+              <div className="relative flex flex-col items-center border-solid border-[5px] border-orange-600 cursor-pointer">
                 <button
-                  className="hover:bg-orange-300 text-black-300 font-bold py-5 px-5 w-full h-full"
-                  onClick={toggleDropdown}
+                  className="hover:bg-orange-300 text-black-300 font-bold py-5 px-5 w-full h-full flex items-center justify-between"
+                  onClick={toggleRestaurantDropdown}
                 >
-                  Select a restaurant
+                  <span className="flex-grow text-center">
+                    Select A Restaurant
+                  </span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="h-4 w-4 ml-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={1.5}
+                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
                 </button>
-              {isOpen && (
-                <div className="absolute top-full mt-1 w-full border-solid border-[1px] border-black-300 bg-white rounded">
-                  <getSelection>
-                    <option value="Restaurant 1">Restaurant 1</option>
-                    <option value="Restaurant 2">Restaurant 2</option>
-                    <option value="Restaurant 3">Restaurant 3</option>
-                    <option value="Restaurant 4">Restaurant 4</option>
-                    <option value="Restaurant 5">Restaurant 5</option>
-                    <option value="Restaurant 6">Restaurant 6</option>
-                    <option value="Restaurant 7">Restaurant 7</option>
-                  </getSelection>
-                </div>
+                {isRestaurantOpen && (
+                  <div className="absolute top-full mt-1 w-full border-solid border-[1px] border-black-300 bg-white rounded z-10">
+                    <ul>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        Restaurant 1
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        Restaurant 2
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        Restaurant 3
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        Restaurant 4
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        Restaurant 5
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        Restaurant 6
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        Restaurant 7
+                      </li>
+                    </ul>
+                  </div>
                 )}
               </div>
-
               {/* Calendar */}
               <div className="calendar">
                 <div className="calendar-content">
@@ -78,37 +120,201 @@ import { useState } from "react";
                 </div>
                 <div className="dates" id="dates"></div>
               </div>
-
               {/* Time Reservation */}
-              <div className="time-reservation-dropdown">
-                <button>Time</button>
-                <select className="TimeAvailableList">
-                  <option value="7:00PM">7:00PM</option>
-                  <option value="11:00PM">11:00PM</option>
-                  <option value="8:00AM">8:00AM</option>
-                </select>
+              <div className="relative flex flex-col items-center border-solid border-[1px] border-black cursor-pointer">
+                <button
+                  className="hover:bg-orange-300 text-black-300 font-bold py-5 px-5 w-full h-full flex items-center justify-between"
+                  onClick={toggleTimeDropdown}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="h-4 w-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={1.5}
+                      d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                  <span className="flex-grow text-center">7:00 PM</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-4 w-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </button>
+                {isTimeOpen && (
+                  <div className="absolute top-full mt-1 w-full border-solid border-[2px] border-black-300 bg-white rounded z-10">
+                    <ul>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        2:00PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        2:30PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        3:00PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        3:30PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        4:00PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        4:30PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        5:00PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        5:30PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        6:00PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        6:30PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        7:00PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        7:30PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        8:00PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        8:30PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        9:00PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        9:30PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        10:00PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        10:30PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        11:00PM
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        11:30PM
+                      </li>
+                    </ul>
+                  </div>
+                )}
               </div>
+              {/* Number of People */}{" "}
+              <div className="relative flex flex-col items-center border-solid border-[1px] border-black cursor-pointer">
+                <button
+                  className="hover:bg-orange-300 text-black-300 font-bold py-5 px-5 w-full h-full flex items-center justify-between"
+                  onClick={toggleGuestDropdown}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-4 w-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                    />
+                  </svg>
+                  <span className="flex-grow text-center">4 people</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="h-4 w-4 ml-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={1.5}
+                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </button>
 
-              {/* Number of People */}
-              <div className="num-of-people-dropdown">
-                <button>Number of Guests</button>
-                <select className="Num-of-people">
-                  <option value="2 people">2 people</option>
-                  <option value="3 people">3 people</option>
-                  <option value="4 people">4 people</option>
-                  <option value="5 people">5 people</option>
-                  <option value="6 people">6 people</option>
-                  <option value="7 people">7 people</option>
-                  <option value="8 people">8 people</option>
-                  <option value="9 people">9 people</option>
-                  <option value="10 people">10 people</option>
-                  <option value="11 people">11 people</option>
-                  <option value="12 people">12 people</option>
-                </select>
+                {isGuestOpen && (
+                  <div className="absolute top-full mt-1 w-full border-solid border-[2px] border-black-300 bg-white rounded z-10">
+                    <ul>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        2 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        3 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        4 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        5 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        6 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        7 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        8 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        9 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        10 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        11 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        12 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        13 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        14 people
+                      </li>
+                      <li className="hover:bg-blue-500 cursor-pointer p-2 rounded">
+                        15 people
+                      </li>
+                    </ul>
+                  </div>
+                )}
               </div>
-
               {/* Submit Button */}
-              <button>Ready to Find</button>
+              <div className="relative flex flex-col items-center border-solid border-[2px] border-black-800 cursor-pointer bg-orange-600">
+                <button className="hover:bg-orange-600 text-white font-bold py-5 px-5 w-full h-full">
+                  READY TO FIND!
+                </button>
+              </div>
             </form>
           </div>
         </div>
